e515f8f0a4d0270425bfab4ebfc023e44092379e
Alamin Khan <alamin@Alamins-Mac-mini.local>
2024-05-19 18:19:43 +0300

    icon-update

--- src/views/Notification/NotificationView.vue
+++ src/views/Notification/NotificationView.vue
@@ -1,3 +1,16 @@
 <template>
-    <h1>Notification Service</h1>
+  <div>
+    <h1 class="header">Notification</h1>
+  </div>
 </template>
+<style scoped>
+.header {
+  padding: 40px;
+  text-align: center;
+  background: #1abc9c;
+  color: white;
+  font-size: 30px;
+  margin-bottom: 2%;
+}
+</style>
+>
